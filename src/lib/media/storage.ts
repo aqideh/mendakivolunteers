1499e39013708b047d9dc7d5768da9a939344831
aqideh <123452394+aqideh@users.noreply.github.com>
2026-09-25 07:57:50 +0800

    Add image storage constants

---
+++ src/lib/media/storage.ts
@@ -0,0 +1,5 @@
+export const profilePhotoBucket = "profile-photos";
+export const eventImageBucket = "event-images";
+
+export const profilePhotoMaxBytes = 512 * 1024;
+export const eventImageMaxBytes = 1024 * 1024;
